4f3ec91c4d46e95d9af1fb598c5457f830e8c897
Andy Marks <amarks@thoughtworks.com>
2012-08-28 10:02:32 +0800

    API now capable of listing runs and inserting new runs via POST.  No validation as yet though.

--- api/lib/api.rb
+++ api/lib/api.rb
@@ -1,25 +1,25 @@
 require 'mongo'
 require 'sinatra'
+require 'json'
 
 @@conn = Mongo::Connection.new
 @@db   = @@conn['toc']
 
 get '/productionline/runs' do
-  "List of runs"
-
   runs = @@db['runs']
 
-  runs.to_s
-  # @@runs.each_with_index do |run, index|
-  #   output << @@runs['number_of_stations']
-  #   output << @@runs['initial_inventory_size']
-  # end
-  # @@runs.count.to_s
-
-  # output
   erb :index, :format => :html, :locals => { :runs => runs }
 end
 
 post '/productionline/run' do
-  201
+  runs = @@db['runs']
+  request.body.rewind
+
+  run = JSON.parse request.body.read
+  logger.info("New run to insert: " + run.to_s)
+  puts("New run to insert: " + run.to_s)
+
+  runs.insert(run) if run
+
+  erb :new, :format => :json, :locals => { :run => run }
 end
